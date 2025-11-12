a01644b71f2e6f9eef1e7e32edb2bcc8852d7107
D.Franklin <franklin_043097@hotmail.com>
2025-11-12 08:55:14 -0500

    create card for every manga

--- frontend/src/view/Pages/Library.tsx
+++ frontend/src/view/Pages/Library.tsx
@@ -1,9 +1,23 @@
 import {  useEffect, useState } from "react"
 import type { Manga } from "../../types"
+import MangaCard from "../../components/lobby/MangaCard"
+import { useForm, type SubmitHandler } from "react-hook-form"
+import { set } from "zod"
+
+type FormsInputs = {
+    search: string
+    sortBy: string
+}
 
 export default function Library () {
 
     const [Mangas , setMangas] = useState<Manga[]>()
+    const { register, handleSubmit } = useForm<FormsInputs>({
+        defaultValues : { 
+            search: "",
+            sortBy: "title-asc"
+        }
+    })
 
     async function fetchMangas () {
         // Lógica para obtener la lista de mangas de la biblioteca del usuario
@@ -11,32 +25,41 @@ export default function Library () {
         try {
 
             const response = await fetch('/public/manga.json')
-
-            console.log( response)
             const data = await response.json()
-
-            console.log( data )
 
             if( data.error ) { 
                 throw new Error(data.message || "Error al cargar la biblioteca")
             }
 
-            setMangas( data.mangas )
-
-            // Procesar y establecer los datos obtenidos    
+            setMangas( data.mangas ) 
             
         } catch (error) {
             console.log( error )
         }
+
     }
 
     useEffect(() => {   
         fetchMangas()
     },[])
 
+    const handleSearch : SubmitHandler<FormsInputs> = ( data ) => {
+        const { search , sortBy } = data
 
-    console.log( Mangas )
-    
+        const filteredMangas = Mangas?.filter( manga => 
+            manga.title.toLowerCase().includes( search.toLowerCase() )
+        )
+
+        filteredMangas && ( setMangas( filteredMangas ) )        
+
+        // Lógica para ordenar los mangas según el criterio seleccionado
+        //let sortedMangas = [...(filteredMangas || [])]
+
+    }
+
+    { console.log(Mangas) }
+
+
     return ( 
 
         <>
@@ -45,14 +68,19 @@ export default function Library () {
                 
                 <div className=" flex content-center space-x-4 py-4">
 
-                    <form className="flex">
+                    <form className="flex" onSubmit={  handleSubmit( handleSearch ) } >
                         <div className="flex items-center space-between space-x-2">
 
-                            <input type="text" placeholder="Buscar...." className="rounded-md p-1 text-black bg-white w-2xs"/>
+                            <input 
+                                type="text" 
+                                placeholder="Buscar...." 
+                                className="rounded-md p-1 text-black bg-white w-2xs"
+                                {...register("search")}
+                            />
 
                             <label> Ordenar por : </label>
 
-                            <select className="bg-white"> 
+                            <select className="bg-white" {...register("sortBy")}> 
                                 <option value="title-asc">Titulo Ascendente</option>
                                 <option value="title-desc">Titulo Descendente</option>
                                 <option value="date-added-asc">Fecha de agregado Ascendente</option>
@@ -62,8 +90,6 @@ export default function Library () {
                             </select>
 
                         </div>
-
-                        
 
                         <input type="submit" value="Buscar" className="bg-secondary rounded-md p-1 ml-2"/>
 
@@ -75,7 +101,7 @@ export default function Library () {
 
             <div className=" flex flex-row gap-4   ">
 
-                <form className="basis-xs bg-gray-400 p-4 rounded-md">
+                <form className="basis-1/4 bg-gray-400 p-4 rounded-md mt-4 h-auto">
 
                     <aside>
                         <div>
@@ -97,7 +123,7 @@ export default function Library () {
 
                 <main className="basis-2/3">
                     {/* Aquí iría la lista de mangas en la biblioteca del usuario */}
-                    <p>Lista de mangas en la biblioteca del usuario</p>
+                    <MangaCard/>
                 </main>
 
 
--- frontend/src/view/Pages/Lobby.tsx
+++ frontend/src/view/Pages/Lobby.tsx
@@ -1,18 +1,52 @@
 import MangaCard from "../../components/lobby/MangaCard";
+import { useEffect, useState } from "react";
+import type { Manga } from "../../types";
 
 export default function Lobby () {
+
+  const [ Mangas, setMangas ] = useState<Manga[]>() 
+
+  async function fetchMangas() {
+      const response = await fetch('./manga.json')
+      const data = await response.json()
+
+      if( data.error )  {
+          throw new Error( data.message || 'Something went wrong' )
+      }
+
+      setMangas( data.mangas )
+  }
+
+  useEffect(() => {
+      
+      fetchMangas()
+
+  }, [])
+
+  if ( !Mangas ) return <div> Loading... </div>
+
   return (
     <>
 
-    <div className=" container  my-8 w-lg">
+    <div className=" container  my-8 w-full ">
       <div>
         <h1 className=" text-subtitle my-2 p-2"> Mas Populares </h1>
       </div>
 
-      <div className=" grid grid-cols-3 gap-4 ">
-        <MangaCard/>
-      </div>
+      <div className=" grid  grid-cols-7 ">
 
+        { Mangas.map( ( manga ) => (
+          
+          <MangaCard
+            key={ manga.id }
+            manga={ manga }
+          />
+          
+        ))}
+        
+      </div>
+      
+      {/* 
       <div>
         <h1 className=" text-subtitle my-2 p-2"> Mangas Tendencias </h1>
       </div>
@@ -35,7 +69,7 @@ export default function Lobby () {
 
       <div className=" grid grid-cols-3 gap-4 ">
         <MangaCard/>
-      </div>
+      </div> */}
 
     </div>
 
